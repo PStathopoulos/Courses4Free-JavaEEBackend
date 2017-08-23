4d65e0d1c1f51d6987d4ac635d3af8fc2ced496d
PStathopoulos <31006665+PStathopoulos@users.noreply.github.com>
2017-08-24 02:18:37 +0300

    Delete KeyGenerator.java

--- Courses4FreeAPI/src/main/java/pnstath/restapis/courses4freeapi/utilities/KeyGenerator.java
+++
@@ -1,9 +0,0 @@
-package pnstath.restapis.courses4freeapi.utilities;
-
-import java.security.Key;
-
-// KeyGenerator Interface that exposes the generateKey method, which is implemented by RestKeyGenerator class
-public interface KeyGenerator {
-
-	 public Key generateKey();
-}
